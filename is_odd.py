6a1a9a38610135ed956818e68b4c196d4ae7fa9e
semccomas <semccomas@gmail.com>
2023-05-30 14:15:49 +0200

    is_odd raises TypeError with str

--- is_odd.py
+++ is_odd.py
@@ -4,7 +4,16 @@ def is_odd(x):
 	Return False if integer is not odd
 	Raise TypeError if x is not an integer
 	"""
-
+	if not isinstance(x, int):
+		raise TypeError("'x' must be an integer")
 	return	
 
 assert is_odd.__doc__ is not None
+
+number=False
+try:
+	is_odd('sdfsdfwerw')
+except TypeError:
+	number=True
+assert(number)	
+
