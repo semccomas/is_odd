203ccc21e91a47bc2b4e0a88ef5afa3473cfb507
semccomas <semccomas@gmail.com>
2023-05-30 14:12:52 +0200

    is_odd has documentation

---
+++ is_odd.py
@@ -0,0 +1,10 @@
+def is_odd(x):
+	"""
+	Return True if integer is odd
+	Return False if integer is not odd
+	Raise TypeError if x is not an integer
+	"""
+
+	return	
+
+assert is_odd.__doc__ is not None
